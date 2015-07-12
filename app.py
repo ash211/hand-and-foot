793b4f92750fbfe2b9fd056f4561306b0b8b368d
Andrew Ash <andrew@andrewash.com>
2015-07-11 17:10:38 -0700

    Add bootstrap and page skeleton

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
